fe2f48f7db9d6b411a16e03785bad6e3229dfd6a
followme2020 <jakob0417@gmail.com>
2020-05-05 02:52:14 +0300

    sweet start

---
+++ app.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+questions = ['Does it sail?', 'It travels over rails?']
+guesses = ['Car', 'Ship', 'Train']
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/question/<int:id>', methods=['GET','POST'])
+def question(id):
+    if request.method == 'POST':
+        if request.form['answer'] == 'yes':
+            return redirect(url_for('question', id=id+1))
+        else:
+            return redirect(url_for('question', id=id))
+    return render_template('question.html', question=questions[id])
+
+
+@app.route('/guess/<int:id>')
+def guess(id):
+    return render_template('guess.html', guess=guesses[id])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
