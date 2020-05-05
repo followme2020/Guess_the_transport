accf6bef046854b7bb53b05daafc6f29a5525bdb
followme2020 <jakob0417@gmail.com>
2020-05-05 22:12:09 +0300

    in this commit Ive been using flask-WTForms. An easy way to control/verify user input aswell as protecting against CSRF attacks. The way its protecting is by ensuring that the GET request is actually issued. It uses a token (random string of caracters, wich are different every time) in the form (in the GET requset when rendering the form). And when POST request submited WTForms verify the token just came in to the one that was sent.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 
 from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import Form
+from wtforms.fields import RadioField, SubmitField
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 questions = ['Does it sail?', 'It travels over rails?']
 guesses = ['Car', 'Ship', 'Train']
+
+
+class YesNoQuestionForm(Form):
+    answer = RadioField('Your answer', choices=[('yes', 'Yes'), ('no', 'No')])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -13,13 +21,13 @@ def index():
 
 @app.route('/question/<int:id>', methods=['GET','POST'])
 def question(id):
-    if request.method == 'POST':
-        if request.form['answer'] == 'yes':
+    form = YesNoQuestionForm()
+    if form.validate_on_submit():
+        if form.answer.data == 'yes':
             return redirect(url_for('question', id=id+1))
         else:
             return redirect(url_for('question', id=id))
-    return render_template('question.html', question=questions[id])
-
+    return render_template('question.html', question=questions[id], form=form)
 
 @app.route('/guess/<int:id>')
 def guess(id):
